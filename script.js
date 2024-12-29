dae76e68ad881bc4d4f54cf9ebcb650710f0c4b5
Segun David <segdavid03@gmail.com>
2024-12-30 00:41:36 +0100

    working with more advanced DOM elements

--- script.js
+++ script.js
@@ -75,3 +75,24 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// overide default movements
+containerMovements.innerHTML = "";
+
+// insert HTML elements from array into movements app
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
