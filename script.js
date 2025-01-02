231f61fe43b5bfc04b1fdacfddaa0647fe71d445
Segun David <segdavid03@gmail.com>
2025-01-02 23:14:09 +0100

    working with chaining methods in js(pipelining)

--- script.js
+++ script.js
@@ -120,10 +120,21 @@ const calcOutSummary = function (movements) {
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}EUR`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}EUR`;
 };
 
 // call function
 calcOutSummary(account1.movements);
+
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
